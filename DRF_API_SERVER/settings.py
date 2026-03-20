86adb02ea10439cd0f46aec94bbe5ae9fe5b6b2c
Roman E <rk2555@yandex.ru>
2026-03-20 09:47:15 +0300

    Simplify CORS_ALLOW_HEADERS

--- DRF_API_SERVER/settings.py
+++ DRF_API_SERVER/settings.py
@@ -128,15 +128,9 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
 
 
-# Разрешить все заголовки
+# Базовые заголовки (можно удалить весь блок, используются значения по умолчанию)
 CORS_ALLOW_HEADERS = [
     'accept',
-    'accept-encoding',
     'authorization',
     'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
 ]
